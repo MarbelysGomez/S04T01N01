3537e7aabbcf7ad394d1c964a691f9212abb0298
Marbelys Gómez <137915980+MarbelysGomez@users.noreply.github.com>
2024-01-17 19:50:14 +0100

    Intro Spring Boot - Maven

---
+++ src/main/java/cat/itacademy/barcelonactiva/gomez/marbelys/s04/t01/n01/S04T01N01GomezMarbelys/controllers/S04T01N01GomezMarbelysApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.gomez.marbelys.s04.t01.n01.S04T01N01GomezMarbelys.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01GomezMarbelysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01GomezMarbelysApplication.class, args);
+
+	}
+}
